8487bc82cb5a36ce9e5a3557cc289c37b3de74a5
David Bolin <david@bolin.at>
2019-03-07 13:29:56 -0800

    added app image and started composing text

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+/*
+
+workspace for intro and bio
+
+Hi. I'm David Bolin, a software engineer currently located in the San Francisco Bay Area.
+
+Programming runs in the family, but I've always identified more as the thinker and philosopher. So I love the magical power of code, which can change the world by mere thought (helped out by a keyboard of course!) I enjoy long walks, books, and good conversations on any and every topic, with the minor exception of small talk. 
+
+I like clean, consise, and readable code, but I'll break any rule that needs breaking to get the job done. Check out a few of my projects below and contact me using the appropriate links. I am currently looking for work.  
+
+---
+
+JavaScript Quiz
+
+This is a little quiz testing the user's knowledge of JavaScript. I created this app in order to showcase basic frontend technologies, namely html, css, and JavaScript. 
+
+Additional technologies: jQuery
+
+Live app: https://thinkful-ei-bee.github.io/quiz-app-james-david/
+
+Source: https://thinkful-ei-bee.github.io/quiz-app-james-david/
+
+*/
