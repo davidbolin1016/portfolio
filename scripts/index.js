b4e5afa07e00256c8104d8d849402e13e1bed55f
David Bolin <david@bolin.at>
2019-03-08 10:55:42 -0800

    minor changes to bio text

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,15 @@ workspace for intro and bio
 
 Hi. I'm David Bolin, a software engineer currently located in the San Francisco Bay Area.
 
-Programming runs in the family, but I've always identified more as the thinker and philosopher. So I love the magical power of code, which can change the world by mere thought (helped out by a keyboard of course!) I enjoy long walks, books, and good conversations on any and every topic, with the minor exception of small talk. 
+Programming runs in the family, but I've always identified more as the thinker and philosopher. As such, I love the magical power of code, which can change the world by mere thought (helped out by a keyboard of course!) I enjoy losing track of time while taking long walks, reading books, or having great conversations on nearly any topic with the exception of random small talk. 
 
-I like clean, consise, and readable code, but I'll break any rule that needs breaking to get the job done. Check out a few of my projects below and contact me using the appropriate links. I am currently looking for work.  
+I like clean, concise, and readable code, but I'll break any rule that needs breaking to get the job done. Check out a few of my projects below and contact me using the appropriate links. I am currently looking for work.  
 
 ---
-
+Project 1
 JavaScript Quiz
 
-This is a little quiz testing the user's knowledge of JavaScript. I created this app in order to showcase basic frontend technologies, namely html, css, and JavaScript. 
+This is a little quiz testing the user's knowledge of JavaScript. I created this app in order to showcase basic frontend technologies, namely html, css, and JavaScript, as well as to confuse the user with the quiz questions on some of JavaScript's weirder behavior. 
 
 Additional technologies: jQuery
 
