5ffc31351e2c2addcce9f97a335b7bf5ac660173
David Bolin <david@bolin.at>
2019-03-08 11:39:19 -0800

    transferred header and bio to html page, styled

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,8 @@
 
 workspace for intro and bio
 
-Hi. I'm David Bolin, a software engineer currently located in the San Francisco Bay Area.
 
-Programming runs in the family, but I've always identified more as the thinker and philosopher. As such, I love the magical power of code, which can change the world by mere thought (helped out by a keyboard of course!) I enjoy losing track of time while taking long walks, reading books, or having great conversations on nearly any topic with the exception of random small talk. 
-
-I like clean, concise, and readable code, but I'll break any rule that needs breaking to get the job done. Check out a few of my projects below and contact me using the appropriate links. I am currently looking for work.  
+ 
 
 ---
 Project 1
