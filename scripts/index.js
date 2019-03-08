235ba92d15931898367001d372067339d402fe41
David Bolin <david@bolin.at>
2019-03-08 13:35:32 -0800

    added all nav bar links, started 1st project

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,9 @@
 
 /*
 
-workspace for intro and bio
-
-
- 
-
 ---
 Project 1
-JavaScript Quiz
+
 
 This is a little quiz testing the user's knowledge of JavaScript. I created this app in order to showcase basic frontend technologies, namely html, css, and JavaScript, as well as to confuse the user with the quiz questions on some of JavaScript's weirder behavior. 
 
