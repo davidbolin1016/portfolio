a4b32ca60d3f87b558c1bfbf40da8943b3717ef1
David Bolin <david@bolin.at>
2019-03-09 09:14:47 -0800

    added autoscroll

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function handleScrollToProjects() {
+  $('#projects-link').click(event => {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $('#project-1').offset().top - 100
+    }, 750);
+  });
+}
+
+$(function() {
+  handleScrollToProjects();
+});
